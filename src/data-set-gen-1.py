710ab542265725627eb86924917403999b32a446
vskdtc <vskdtc@10.0.1.29>
2018-03-29 18:05:52 -0700

    new test code

---
+++ src/data-set-gen-1.py
@@ -0,0 +1,6 @@
+# Generate a dataset and plot it
+import numpy as np
+import sklearn
+np.random.seed(0)
+X, y = sklearn.datasets.make_moons(200, noise=0.20)
+plt.scatter(X[:,0], X[:,1], s=40, c=y, cmap=plt.cm.Spectral)
